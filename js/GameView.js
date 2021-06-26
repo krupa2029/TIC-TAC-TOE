d5b1d56a8f1f063abd48e8479960e6f5b3cfb348
krupa2029 <kjkotia10@gmail.com>
2021-06-26 02:04:23 -0700

    First commit

---
+++ js/GameView.js
@@ -0,0 +1,15 @@
+export default class GameView{
+
+    constructor(){
+        console.log("init gameview");
+    }
+
+    updateBoard(game){
+        console.log("update gameview")
+        console.log(game.board);
+        for(let i=0; i< game.board.length; i++){
+            const tile = document.querySelector(`.board-tile[data-index='${i}']`);
+            tile.textContent = game.board()
+    }
+}
+}
---
+++ js/main.js
@@ -0,0 +1,37 @@
+import Game from "./Game.js"
+import GameView from "./GameView.js"
+
+let game = new Game();
+let gameView = new GameView();
+// gameView.updateBoard(game);
+
+
+// console.log(game.board);
+// console.log("My turn:", game.turn);
+// game.nextTurn();
+// console.log("My turn:", game.turn);
+// game.makeMove(3);
+// console.log(game.board);
+// game.nextTurn();
+// game.makeMove(5);
+// console.log(game.board);
+// gameView.updateBoard(game);
+
+let tiles =  document.querySelectorAll(".board-tile");
+tiles.forEach((tile) =>{
+    tile.addEventListener("click", ()=> {
+        onTileClick(tile.dataset.index);
+    })
+})
+
+function onTileClick(i){
+    //do something
+    // make a move
+    game.makeMove(i);
+    // change turn
+
+    // update board
+    gameView.updateBoard(game);
+    game.nextTurn();
+
+}
